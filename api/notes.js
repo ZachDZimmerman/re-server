c1d0abe2eadc3d3902f2631a528a1badef134ffd
ZachZimmerman7 <zzimmerman@gmail.com>
2016-10-18 12:02:32 -0600

    Working localhost server

--- api/notes.js
+++ api/notes.js
@@ -1,7 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var knex = require('../db/knex');
-var queries = require('../db/queries');
+var queries = require("../db/queries");
+
+router.get("/", function(req, res, next) {
+    if (req.query.query) {
+        queries.searchingNotes(req.query.query).then(function(data) {
+            res.json({data});
+        });
+    } else {
+        next();
+    }
+});
+
+router.get("/", function(req, res, next) {
+    queries.getAllNotes().then(function(notes) {
+        res.json({notes});
+    }).catch(function(error) {
+        next(error);
+    });
+});
+
+router.post("/", function(req, res, next) {
+    queries.addNote(req.body).then(function(note) {
+        res.json({ note });
+    }).catch(function(error) {
+        next(error);
+    });
+});
+
+router.get("/:id", function(req, res, next) {
+    queries.getOneNote(req.params.id).then(function(data) {
+        res.json({ data });
+    }).catch(function(error) {
+        next(error);
+    });
+});
+
 
 
 module.exports = router;
---
+++ db/queries.js
@@ -0,0 +1,18 @@
+/*eslint-disable*/
+
+var knex = require('../db/knex');
+
+module.exports = {
+    addNote: function(body) {
+        return knex('notes').insert(body).returning('*');
+    },
+    getAllNotes: function() {
+        return knex('notes').select();
+    },
+    getOneNote: function(id) {
+        return knex('notes').where({id: id});
+    },
+    searchingNotes: function(body) {
+      return knex('notes').select().where('body', 'like', '%'+query+'%');
+    }
+};
